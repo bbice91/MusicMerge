2f8186b68528ebec61c838f6752e7a76b575f425
Rachel Goode <rgoode92@gmail.com>
2022-05-23 21:10:12 -0400

    attempt create user component/users service

--- movie-merge-front-end/src/app/users.service.ts
+++ movie-merge-front-end/src/app/users.service.ts
@@ -1,11 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Users } from './models/Users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor(private httpClient: HttpClient) { }
+
+
+  constructor(router: Router, _usersService: UsersService, httpClient: HttpClient) { }
   baseUrl = "https://localhost:7265/users";
+
+  // CreateUser(userName: Users, password: Users) {
+  //   return this.httpClient.post<Users>(this.baseUrl, userName, password);
+  // }
 }
+
+
+
+
+
+
