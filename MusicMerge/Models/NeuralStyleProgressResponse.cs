d756179053e6f8059b3570b77dfed7d967f1fbc2
Rachel Goode <rgoode92@gmail.com>
2022-05-28 22:19:19 -0400

    Recommitting 53eeb4a2e1fed5d7d1cf9ea56ba5ea19b4bbbef1

--- MusicMerge/Models/NeuralStyleProgressResponse.cs
+++ MusicMerge/Models/NeuralStyleProgressResponse.cs
@@ -4,7 +4,7 @@ namespace MusicMerge
 {
     public class NeuralStyleProgressResponse
     {
-        public string result {  get; set; }
+        public string? result {  get; set; }
         public string status { get; set; }
         public int progress { get; set; }
         public string? url { get; set; }
