bbe09e9d1c0a68b0a6c9403990b057c0df1812f0
Leon Wilke <leonwilke1991@gmail.com>
2022-06-06 00:26:20 -0400

    no bugs except for 1 line in the oauth controller, I think i have everything? might be missing 1 thing

---
+++ MusicMerge/Controllers/OAuthController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MusicMerge.Models;
+using MusicMerge.Services;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using MusicMerge;
+
+
+
+namespace MusicMerge.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class OAuthController : ControllerBase
+    {
+        private readonly SpotifyService _spotifyService;
+        private readonly UserContext _userContext;
+
+        public OAuthController(UserContext userContext, SpotifyService spotifyService)
+        {
+            _spotifyService = spotifyService;
+            _userContext = userContext;
+        }
+
+        //[HttpGet]
+        //[Route("/authorize")]
+        //public async Task<IActionResult> Login([FromRoute] string code, [FromRoute] Authorize authorize)
+        //{
+        //    var token = authorize switch
+        //    {
+        //        Authorize.Spotify => await _spotifyService.GetToken(code),
+        //    };
+
+        //    var spotifyUser = await _spotifyService.GetSpotifyUser(token.AccessToken);
+
+        //    if (spotifyUser == null)
+        //    {
+        //        return Unauthorized();
+        //    }
+
+        //    var user = await _userContext.UpsertSpotifyUser(spotifyUser, token.AccessToken);
+
+        //    return Ok(user);
+        //}
+
+        //[HttpPost]
+        //[Route("/api/token")]
+
+    }
+}
