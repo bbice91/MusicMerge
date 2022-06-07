719fd02f6fbe2a38931816ae089834b6d857a07d
Leon Wilke <leonwilke1991@gmail.com>
2022-06-06 22:00:55 -0400

    backend oauth pt 100, think i'm just missing the configuration?

---
+++ MusicMerge/Controllers/OAuthController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MusicMerge.Models;
+using MusicMerge.Services;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using MusicMerge;
+using System.Text;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace MusicMerge.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class OAuthController : ControllerBase
+    {
+        private readonly SpotifyService _spotifyService;
+        private readonly UserContext _userContext;
+
+        public OAuthController(UserContext userContext, SpotifyService spotifyService)
+        {
+            _spotifyService = spotifyService;
+            _userContext = userContext;
+        }
+
+        [HttpGet]
+        [Route("/authorize")]
+        public async Task<IActionResult> Login([FromRoute] string code, [FromRoute] Authorize authorize)
+        {
+            var token = authorize switch
+            {
+                Authorize.Spotify => await _spotifyService.GetToken(code),
+            };
+
+            var spotifyUser = await _spotifyService.GetSpotifyUser(token.AccessToken);
+
+            if (spotifyUser == null)
+            {
+                return Unauthorized();
+            }
+
+            await _userContext.UpsertSpotifyUser(spotifyUser, token.AccessToken);
+
+            return Ok();
+        }
+
+        //[HttpPost]
+        //[Route("/api/token")]
+
+        //public async Task<IActionResult> GetAccess([FromRoute] string accessToken, string grantType)
+        //{
+
+        //}
+    }
+}
---
+++ MusicMerge/Models/SpotifyUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MusicMerge.Models
+{
+    public class SpotifyUser
+    {
+        public string id { get; set; }
+        public string displayName { get; set; }
+
+        public DateTime createdDate { get; set; }
+
+        public string email { get; set; }
+        public object externalUrls { get; set; }
+
+        public string product { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+        public object followers { get; set; }
+        public string login { get; set; }
+        public string webClient { get; set; }
+
+
+
+       
+
+    }
+}
